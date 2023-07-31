9be2c6639e9570f9c22832e3d89cd46ced8286b8
AlexxFuentes <aafuentes@unah.hn>
2023-07-30 19:23:41 -0600

    Avance tareas 4

--- tarea_5_S8/core/HTMLParser.py
+++ tarea_5_S8/core/HTMLParser.py
@@ -1,10 +1,30 @@
 import re
 
 class HTMLParser: 
-    
+   
     def __init__(self, html):
         self.html = html
     
+    
+    def find_html_tags(self, html: str) -> list:
+        """
+            Identifica etiquetas HTML en una cadena, 
+            retorna una lista con las etiquetas HMTL identificadas
+        """
+        regex = r"\<([a-zA-Z0-9/=#//:.\'\" _-]+\s*)*\>"
+        return re.findall(regex, html)
+
+    def run(self):
+        """
+            Función main de la clase
+        """    
+        content = self.html.split("\n")
+        
+        for line in content: 
+            tags = self.find_html_tags(line)
+            print("TAGS")
+            result = [ self.process_attributes(tag.strip()) for tag in tags ]
+            print("\n".join(result))
 
     def find_html_tags(self, html:str) -> list:
         """
